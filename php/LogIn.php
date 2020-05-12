6ec70ab547154cc1ab9752181dea4a850e5fb38d
epinefrin1921 <nedim.ajdin@stu.ssst.edu.ba>
2020-05-12 13:35:07 +0200

    Responsive added to Login and Register

--- php/LogIn.php
+++ php/LogIn.php
@@ -2,13 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
     <title>Log In</title>
     <link rel="stylesheet" href="LogInStil.css">
     <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
-<h1>Welcome to orderly</h1>
+<h1><a href="index.php">Welcome to orderly</a></h1>
 <div class="Login">
     <h2>Log in</h2>
     <div class="textbox">
@@ -20,13 +20,6 @@
 
     <input class="butt" type="button" name="" value="Sign In">
 </div>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<?php include 'footer.php'; ?>
+
 </body>
 </html>
--- php/Register.php
+++ php/Register.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,37 +9,39 @@
     <link rel="stylesheet" href="RegStil.css">
 </head>
 <body>
-<h1>Welcome to orderly</h1>
+<h1><a href="index.php">Welcome to orderly</a></h1>
+<?php
+$today = date("Y-m-d");
+?>
 <form class="Login" action="insert.php" method="post">
     <h2>Register</h2>
     <div class="textbox">
-        <input type="text" placeholder="Name" name="FName" value="">
+        <input type="text" placeholder="Name" name="FName" value="" required>
     </div>
     <div class="textbox">
-        <input type="text" placeholder="Surname" name="LName" value="">
+        <input type="text" placeholder="Surname" name="LName" value="" required>
     </div>
     <div class="textbox" id="date1">
-        <input type="date" placeholder="Date of Birth" name="DOB" value="">
+        <input type="date" placeholder="Date of Birth" name="DOB" value="" required min="1900-01-01" max="<?php echo $today?>">
+
     </div>
     <div class="textbox">
-        <input type="text" placeholder="E-mail" name="Email" value="">
+        <input type="text" placeholder="E-mail" name="Email" value="" required >
     </div>
     <div class="textbox">
-        <input type="password" placeholder="New Password" name="Password" value="">
+        <input type="password" placeholder="New Password" name="Password" value="" required>
     </div>
     <div class="textbox">
-        <input type="password" placeholder="Confirm Password" name="ConfirmPassword" value="" >
+        <input type="password" placeholder="Confirm Password" name="ConfirmPassword" value="" required >
     </div>
     <input class="butt" type="submit" name="" value="Submit" >
 </form>
-
-
-    <?php include 'footer.php'; ?>
-    <?php
-    $user='root';
-    $pass='';
-    $db='project';
-    $db= new mysqli('localhost',$user,$pass,$db) or die("Unable to connect!");
-    ?>
+<?php include 'footer.php'; ?>
+<?php
+$user='root';
+$pass='';
+$db='project';
+$db= new mysqli('localhost',$user,$pass,$db) or die("Unable to connect!");
+?>
 </body>
 </html>
