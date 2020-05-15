bad7af878cd7ae9c60375b329d61c3b1571b146e
epinefrin1921 <nedim.ajdin@stu.ssst.edu.ba>
2020-05-15 09:22:43 +0200

    Added Combo CRUD, added URL for the products and combos

--- save_product.php
+++ save_product.php
@@ -11,8 +11,8 @@ if ($_POST) {
 
 
 
-    $query = oci_parse($conn, "INSERT INTO MENU_ITEMS (MI_NAME, MI_PRICE, MI_DESCRIPTION, MI_SUPPLY_PRICE, MI_IMG) 
-                      VALUES ('{$name}', {$price},'{$description}',{$price2},'{$image}')");;
+    $query = oci_parse($conn, "INSERT INTO MENU_ITEMS (MI_NAME, MI_PRICE, MI_DESCRIPTION, MI_SUPPLY_PRICE, MI_IMG, MI_TYPE) 
+                      VALUES ('{$name}', {$price},'{$description}',{$price2},'{$image}','single')");;
     oci_execute($query);
     oci_commit($conn);
 
