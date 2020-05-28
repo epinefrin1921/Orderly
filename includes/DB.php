bc0b1268aef7cd9246a57b9dba0a4106dabaea76
epinefrin1921 <nedim.ajdin@stu.ssst.edu.ba>
2020-05-28 16:06:41 +0200

    Updated for cloud

--- includes/DB.php
+++ includes/DB.php
@@ -1,4 +1,5 @@
 
 <?php
-$conn = oci_connect('system', 'pass123', '//localhost/XE');
+/*$conn = oci_connect('system', 'pass123', '//localhost/XE');*/
 
+$conn = oci_connect('admin', 'Orderly123456', 'orderlydb_high');
