c75c28caccfb73d58d2d0477579c8104fdafda7a
epinefrin1921 <nedim.ajdin@stu.ssst.edu.ba>
2020-06-01 14:20:09 +0200

    Email testing

--- validation/PasswordForgot.php
+++ validation/PasswordForgot.php
@@ -19,10 +19,12 @@ $title = 'Password recovery';
 <div class="Login">
     <h2>Password recovery</h2>
     <p>Enter your email address below. You will get mail from our support with instructions how to recover you password.<br><br> Thank you for using our site!</p>
-    <div class="textbox">
-        <input type="text" placeholder="E-mail" name="" value="">
-    </div>
-    <input class="butt" type="submit" name="" value="Submit">
+    <form action="sendEmail.php" method="post">
+        <div class="textbox">
+            <input type="text" placeholder="E-mail" name="" value="">
+        </div>
+        <input class="butt" type="submit" name="" value="Submit">
+    </form>
 </div>
 </body>
 </html>
---
+++ validation/sendEmail.php
@@ -0,0 +1,11 @@
+<?php
+$to      = 'nedim.ajdin@gmail.com';
+$subject = 'Test';
+$message = 'hello test 123';
+$headers = 'From: Orderly' . "\r\n" .
+    'Reply-To: nedim.ajdin@stu.ssst.edu.ba' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+header("location: LogIn.php");
+?> 
