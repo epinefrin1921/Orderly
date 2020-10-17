d7f1f3cb21197b23bfa7bece57ef8653844cf900
epinefrin1921 <65129010+epinefrin1921@users.noreply.github.com>
2020-10-17 17:01:50 +0200

    Update newvisit.php

--- visits/newvisit.php
+++ visits/newvisit.php
@@ -38,10 +38,10 @@ if(isset($_SESSION['id'])){
     <?php endif; ?>
     <form class="Login" action="submitVisit.php" method="post" name="regform">
         <div class="textbox">
-            <input type="text" placeholder="Name" name="fname" value="<?php if($_SESSION['id']){echo $row["c_fname"];}; ?>" >
+            <input type="text" placeholder="Name" name="fname" value="<?php if(isset($_SESSION['id'])){echo $row["c_fname"];}; ?>" >
         </div>
         <div class="textbox">
-            <input type="text" placeholder="Surname" name="lname" value="<?= isset($_SESSION['id'])? $row[1] : null ?>" >
+            <input type="text" placeholder="Surname" name="lname" value="<?= isset($_SESSION['id'])? $row['c_lname'] : null ?>" >
         </div>
         <div class="textbox">
             <input type="email" placeholder="E-mail" name="email" value="<?= isset($_SESSION['id'])? $row[2] : null ?>" >
