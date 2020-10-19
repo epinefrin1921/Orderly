d5b979343fe26a346f607c2a4a80757f4893dbd6
epinefrin1921 <nedim.ajdin@stu.ssst.edu.ba>
2020-10-19 13:05:05 +0200

    Fixed bugs

--- visits/submitTest.php
+++ visits/submitTest.php
@@ -17,7 +17,8 @@ if ($_POST) {
     $phone = $_POST['phone'];
     $query2 = oci_parse($conn, "select max(T_ID) from TESTS");
     oci_execute($query2);
-    $max = oci_fetch_assoc($query2)[0];
+    $row = oci_fetch_assoc($query2);
+    $max = $row["MAX(T_ID)"];
     $max++;
 
     $query = oci_parse($conn, "INSERT INTO TESTS(T_ID, T_FNAME, T_LNAME, T_EMAIL, T_PHONE, T_DATE, T_RESULT)
@@ -30,7 +31,8 @@ if ($_POST) {
 
     $query2 = oci_parse($conn, "select max(N_ID) from NOTIFICATIONS");
     oci_execute($query2);
-    $max = oci_fetch_assoc($query2)[0];
+    $row = oci_fetch_assoc($query2);
+    $max = $row["MAX(N_ID)"];
     $max++;
 
     while ($row = oci_fetch_assoc($query2)) {
